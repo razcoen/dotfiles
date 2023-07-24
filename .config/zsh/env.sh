8e3df1353cc778621453b38c0ca5e0c6b4e7cd3c
razcoen <raz@icohens.com>
2023-07-25 00:20:04 +0300

    fix(zsh): shellcheck

--- .config/zsh/env.sh
+++ .config/zsh/env.sh
@@ -4,17 +4,17 @@ export PATH=$PATH:$HOME/bin
 export NVM_DIR=$HOME/.nvm
 
 # Go
-export GOPATH=$(go env GOPATH)
+GOPATH=$(go env GOPATH)
+export GOPATH
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
 # Git editor (neovim)
-# About neovim remote: https://github.com/mhinz/neovim-remote
 export VISUAL=nvim
 
 # Oh My Zsh 
 export ZSH=$HOME/.oh-my-zsh
 export ZSH_THEME="lambda-gitster" # https://github.com/ergenekonyigit/lambda-gitster
 
-# getopt
+# GNU getopt
 export PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
